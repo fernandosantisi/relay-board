8d68e200309b8dcf42220d20657755f09568897c
fersantisi <you@example.comfer.santisi@hotmail.com>
2023-07-25 13:45:56 -0300

    fix

--- send.js
+++ send.js
@@ -1,38 +1,19 @@
 const { SerialPort } = require('serialport')
+const port = new SerialPort({ path: 'COM1', baudRate: 9600 }, function (err) {
+  if (err) {
+    return console.log('Error: ', err.message)
+  }
+})
 
-const port = '/dev/ttyUSB1';
+const buffer = Buffer.from([9])
 
-const buffer = Buffer.from([0b11111111])
+setTimeout(() => {
 
-console.log(buffer)
-
-const com = new SerialPort({
-  path: port,
-  baudRate: 19200,
-  autoOpen: false,
-});
-
-
-com.on('open', function () {
-  com.write(buffer, function (err) {
+  port.write(buffer, function(err) {
     if (err) {
-      return console.log("Error on write: ", err.message);
+      return console.log('Error on write: ', err.message)
     }
-
-    console.log("Message sent successfully");
-    // Wait for some time (e.g., 100ms) and then close the port
-    setTimeout(() => {
-      com.close((err) => {
-        if (err) {
-          console.error('Error closing the port:', err.message);
-        } else {
-          console.log('Port closed successfully.');
-        }
-      });
-    }, 100);
-  });
-});
-
-com.on('error', function (err) {
-  console.error("Error on opening the port: ", err.message);
-});
+    console.log(buffer)
+  })
+  port.close()
+}, "1000");
